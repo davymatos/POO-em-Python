c1814bb4c62bb4e48ccd1ddce826bafbd3517683
Davy de Matos <davymatos01@gmail.com>
2020-03-12 22:23:50 -0300

    Criando atributos

---
+++ Exemplo 2/main.py
@@ -0,0 +1,5 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Luiz', 29)
+print(p1.nome, p1.idade)
+#print("Seu nome é: {}  e sua idade é: {}".format(p1.nome, p1.idade))
---
+++ Exemplo 2/pessoa.py
@@ -0,0 +1,4 @@
+class Pessoa(object):
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
